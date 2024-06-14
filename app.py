d9b7bd475ac6cc46ed2273e720e12e5c216fcb6b
yx <17879765153@163.com>
2024-06-14 22:02:14 +0800

    1.1

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ from flask import Flask,url_for,redirect,render_template
 
 # print(":__name__:",__name__)
 app = Flask(__name__)
-
+# app = Flask(__name__,template_folder="")  # template_folder 参数可以指定flask应用寻找模板文件的地址
+# print("app.template_folder",app.template_folder)
 
 @app.route('/')
 def hello_world():  # put application's code here
@@ -12,7 +13,7 @@ def hello_world():  # put application's code here
 def loginview():
 
 
-    return render_template("index.html")
+    return render_template("index.html",name="ytw")
 
 
 if __name__ == '__main__':
